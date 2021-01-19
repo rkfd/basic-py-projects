dac7ae845b2b652d51ea313f169a6ad1385a7ee7
Satwik Gawand <satwikgawand@gmail.com>
2021-01-19 16:52:43 +0530

    madlibs project done

---
+++ 1-Madlibs/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+  File name: app.py
+  Author: Satwik Gawand
+  Date Created: 19/01/2021
+  Date Last Modified: 19/01/2021
+  Python Version: 3.8.6
+'''
+
+adj_1 = input("Adjective: ")
+verb_1 = input("Verb: ")
+verb_2 = input("Verb: ")
+famous_person_1 = input("Famous Person: ")
+
+madlib_output = f"Today is a {adj_1} day! I'd like to {verb_1} and I also want to {verb_2}. I feel like {famous_person_1} for some reason."
+
+print(f"\nMADLIB OUTPUT:\n{madlib_output}")
